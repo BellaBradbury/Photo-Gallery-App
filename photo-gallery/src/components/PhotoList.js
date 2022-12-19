4581ea839ec5a0e1bad177968dc4ab0a2dda42cd
BellaBradbury <bellabradbury@yahoo.com>
2022-12-19 04:04:18 -0500

    photolist created

--- photo-gallery/src/components/PhotoList.js
+++ photo-gallery/src/components/PhotoList.js
@@ -1,1 +1,12 @@
 import React from "react";
+import Photo from "./Photo";
+
+const PhotoList = props => {
+    return(
+        <ul>
+            <Photo />
+        </ul>
+    );
+}
+
+export default PhotoList;
