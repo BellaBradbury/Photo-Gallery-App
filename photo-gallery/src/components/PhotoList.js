214286356258f0a29410df64b7a4dc2b34d72310
BellaBradbury <bellabradbury@yahoo.com>
2022-12-19 05:05:23 -0500

    photolist urls updated

--- photo-gallery/src/components/PhotoList.js
+++ photo-gallery/src/components/PhotoList.js
@@ -2,9 +2,11 @@ import React from "react";
 import Photo from "./Photo";
 
 const PhotoList = props => {
+    const results = props.data;
+    let photos = results.map(photo => <Photo url={photo.images.fixed_height.url} key={photo.id} />);
     return(
         <ul>
-            <Photo />
+            {photos}
         </ul>
     );
 }
