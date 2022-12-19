f219bdb7bf5eb760ce0843c360482a55908b99e2
BellaBradbury <bellabradbury@yahoo.com>
2022-12-19 04:51:33 -0500

    app.js updated

--- photo-gallery/src/App.js
+++ photo-gallery/src/App.js
@@ -1,21 +1,27 @@
 import React, { useEffect, useState } from 'react';
+import apiKey from './config';
+
+// import MainNav 
+import SearchForm from './components/SearchForm';
+import PhotoList from './components/PhotoList';
 
 function App() {
+  const [photos, setPhoto] = useState([]);
+  useEffect( () => {
+    fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}=piano&per_page=24&format=json&nojsoncallback=1`)
+      .then( response => response.json() )
+      .then( responseData => setPhoto(responseData.data) )
+      .catch(error => console.log('Error fetching and parsing request', error) );
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <SearchForm />
+      {/* MainNav */}
+      <div className='photo-container'>
+        <h2>Results</h2>
+        <PhotoList />
+      </div>
     </div>
   );
 }
