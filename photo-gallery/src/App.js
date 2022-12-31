61d17862c3b05a6eb36886a4470827ea54da9f95
BellaBradbury <bellabradbury@yahoo.com>
2022-12-30 23:52:13 -0500

    pg not found added

--- photo-gallery/src/App.js
+++ photo-gallery/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import Header from './components/Header';
 import Results from './components/Results';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
             <Route path="/guitar" element={<Results query="guitar" />} />
             <Route path="/drums" element={<Results query="drums" />} />
             <Route path="/:searchText" element={<Results />} />
+            <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </BrowserRouter>
---
+++ photo-gallery/src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+    return (
+        <div className="photo-container">
+            <h2>Uh oh!</h2>
+            <p>The page you're looking for does not exist</p>
+        </div>
+    )
+};
+
+export default NotFound;
